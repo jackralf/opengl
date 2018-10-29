98361e6e19d7bc7c3cb6fe988ee647f61f61dd4a
DESKTOP-15RO263\machenfei <machenfei@gamil.com>
2018-10-29 09:05:07 +0800

    base

--- OpenGL/OpenGL/src/Application.cpp
+++ OpenGL/OpenGL/src/Application.cpp
@@ -1,6 +1,16 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
 using namespace std;
+
+GLuint createProgram();
+bool compileShader(GLenum type, GLuint *shader, const GLchar *source);
+
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
@@ -30,18 +40,47 @@ int main(void)
 
 	glfwSetKeyCallback(window, key_callback);
 
+	// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+	glewExperimental = GL_TRUE;
+	// Initialize GLEW to setup the OpenGL Function pointers
+	glewInit();
+
 	GLfloat vertices[] = {
 		-0.5f, -0.5f, 0.0f,
 		 0.5f, -0.5f, 0.0f,
 		 0.0f,  0.5f, 0.0f
 	};
 
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), 0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	GLuint progrom = createProgram();
+	glUseProgram(progrom);
+
+	GLint location = glGetUniformLocation(progrom, "u_Color");
+	
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		glClearColor(1.0f, 0.0f, 0.0f, 1.0);
+
+
+		glUniform4f(location, 0.8f, 0.2f, 0.3f, 1.0f);
+
+
+		//glBindVertexArray(VAO);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glBindVertexArray(0);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
@@ -52,4 +91,89 @@ int main(void)
 
 	glfwTerminate();
 	return 0;
+}
+
+struct ShaderProgramSource
+{
+	string vertexSource;
+	string fragmentSource;
+};
+
+static ShaderProgramSource parseShader(const string& filePath)
+{
+	ifstream stream(filePath);
+
+	enum class ShaderType
+	{
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+
+	string line;
+	std::stringstream ss[2];
+	ShaderType type = ShaderType::NONE;
+	while (getline(stream, line))
+	{
+		if (line.find("#shader") != string::npos) {
+			if (line.find("vertex") != string::npos)
+				type = ShaderType::VERTEX;
+			else if (line.find("fragment") != string::npos)
+				type = ShaderType::FRAGMENT;
+		} 
+		else {
+			ss[(int)type] << line << '\n';
+		}
+	}
+
+	return { ss[0].str(), ss[1].str() };
+}
+
+
+GLuint createProgram()
+{
+	GLuint program = glCreateProgram();
+	GLuint vertexShader, fragmentShader;
+
+	ShaderProgramSource source = parseShader("res/shaders/Basic.shader");
+	compileShader(GL_VERTEX_SHADER, &vertexShader, source.vertexSource.c_str());
+	compileShader(GL_FRAGMENT_SHADER, &fragmentShader, source.fragmentSource.c_str());
+
+	glAttachShader(program, vertexShader);
+	glAttachShader(program, fragmentShader);
+	glLinkProgram(program);
+
+	GLint success;
+	GLchar infoLog[512];
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(program, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	return program;
+}
+
+bool compileShader(GLenum type, GLuint *shader, const GLchar *source)
+{
+	if (shader == NULL)
+		return false;
+
+	*shader = glCreateShader(type);
+	glShaderSource(*shader, 1, &source, NULL);
+	glCompileShader(*shader);
+
+	GLint status;
+	GLchar infoLog[512];
+	glGetShaderiv(*shader, GL_COMPILE_STATUS, &status);
+	if (!status)
+	{
+		glGetShaderInfoLog(*shader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+
+		return false;
+	}
+
+	return (status == GL_TRUE);
 }
